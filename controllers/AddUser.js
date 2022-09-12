6e56cf1f277187138d1f0173408829e98214187e
bobh <1551608379@qq.com>
2022-09-12 21:59:43 +0800

    fix error handler

--- controllers/AddUser.js
+++ controllers/AddUser.js
@@ -1,7 +1,7 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 
-const addUser = async(req,res) => {
+const addUser = async(req,res,next) => {
     try{
         console.log(req.query);
         let { email, password, username } = req.query;
@@ -15,8 +15,7 @@ const addUser = async(req,res) => {
         await newUser.save();
         res.send("user created!");
     }catch(err){
-        console.log(err);
-        res.status(500).send("error!");
+        next(err);
     }
 }
 
